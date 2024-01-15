e770c0db44a0694affd22b7c66b984cb58d2697f
pildrums <there7788@gmail.com>
2024-01-16 02:37:26 +0900

    Feat: 답글 리스트 컴포넌트 작업 시작

---
+++ src/components/reply/ReplyList.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import "./ReplyList.scss";
+
+function ReplyList() {
+  return (
+    <>
+      <ul className="reply-list-block">
+        <li className="reply-list-block__item">리스트</li>
+      </ul>
+    </>
+  );
+}
+
+export default ReplyList;
