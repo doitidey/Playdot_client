b36abb189725324f2c7c83504f90a493a9a1c7bf
doitidey <evxrafter@gmail.com>
2024-01-18 01:00:33 +0900

    Feat: 기본 파일 생성

---
+++ src/components/signup/SignupSection.tsx
@@ -0,0 +1,13 @@
+function SignupSection() {
+  return (
+    <section>
+      <h1>나의 구단 선택하기</h1>
+      <p>
+        내가 응원하는 구단을 골라주세요! 팀 뱃지를 부여받고 커뮤니티에서 소통할
+        수 있어요.
+      </p>
+    </section>
+  );
+}
+
+export default SignupSection;
