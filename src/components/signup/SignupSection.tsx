e0a3b90fe2249498d3055c49c0d41e503a8637d2
doitidey <evxrafter@gmail.com>
2024-01-18 18:33:35 +0900

    Feat: 회원가입 메인 페이지 작업 및 팀 카드 추가

--- src/components/signup/SignupSection.tsx
+++ src/components/signup/SignupSection.tsx
@@ -1,11 +1,24 @@
+import "@/components/signup/SignupSection.scss";
+import SignupTeamCards from "./SignupTeamCards";
+import { TEAMS_INFO } from "./TeamsInfo";
+import Title from "@/components/common/Title";
+
 function SignupSection() {
   return (
-    <section>
-      <h1>나의 구단 선택하기</h1>
-      <p>
-        내가 응원하는 구단을 골라주세요! 팀 뱃지를 부여받고 커뮤니티에서 소통할
-        수 있어요.
-      </p>
+    <section className="signup-block">
+      <div className="signup-block__intro">
+        <div className="signup-block__intro__circles">1</div>
+        <Title largest>나의 구단 선택하기</Title>
+        <Title small className="signup-block__intro__content">
+          내가 응원하는 구단을 골라주세요!
+          <br />팀 뱃지를 부여받고 커뮤니티에서 소통할 수 있어요.
+        </Title>
+      </div>
+      <div className="teamcards-block">
+        {TEAMS_INFO.map((team) => (
+          <SignupTeamCards key={team.name} {...team} />
+        ))}
+      </div>
     </section>
   );
 }
