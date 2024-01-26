1e9e80d5de1366dfe3a4d6ddc702055be0e44559
doitidey <evxrafter@gmail.com>
2024-01-27 01:08:14 +0900

    feature: hover 효과 적용

--- src/components/signup/SignupTeamCards.tsx
+++ src/components/signup/SignupTeamCards.tsx
@@ -1,15 +1,46 @@
+import React from "react";
 import Image from "next/image";
+import classNames from "classnames";
 import "@/components/signup/SignupTeamCards.scss";
 
-function SignupTeamCards(team: any) {
+interface TeamProps {
+  img: string;
+  name: string;
+  color: string;
+}
+
+function SignupTeamCards(team: TeamProps) {
   return (
     <div className="teamcard-block">
-      <Image src={team.img} alt={team.name} width={0} height={0} />
-      <p className="teamname">{team.name}</p>
-      <div className="teamcard-block__backgrounds">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+      <div className="teamcard-content teamcard-content--active">
+        <p className="teamname">선택하기</p>
+      </div>
+      <div className="teamcard-content">
+        <Image src={team.img} alt={team.name} width={0} height={0} />
+        <p className="teamname">{team.name}</p>
+        <div className="teamcard-content__backgrounds">
+          <div
+            className={classNames(
+              "square",
+              "first-square",
+              `square--${team.color}`,
+            )}
+          ></div>
+          <div
+            className={classNames(
+              "square",
+              "second-square",
+              `square--${team.color}`,
+            )}
+          ></div>
+          <div
+            className={classNames(
+              "square",
+              "third-square",
+              `square--${team.color}`,
+            )}
+          ></div>
+        </div>
       </div>
     </div>
   );
