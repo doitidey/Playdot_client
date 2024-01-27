a17be61fc2668d961cc5b927a2c094dbaa75507d
doitidey <evxrafter@gmail.com>
2024-01-27 14:25:34 +0900

    Design: scss 연결 및 클래스 네임 변경

--- src/components/signup/SignupStepTwo.tsx
+++ src/components/signup/SignupStepTwo.tsx
@@ -8,24 +8,30 @@ import { TEAMS_INFO } from "./TeamsInfo";
 
 function SignupStepTwo() {
   return (
-    <div>
+    <div className="stepTwo-block">
       <Title largest className="content">
         프로필 설정하기
       </Title>
       <Title small className="content">
         나만의 프로필을 만들어봐!
       </Title>
-      <div className="teamcards-block">
+      <div className="stepTwo-content">
         <SignupTeamCards {...TEAMS_INFO[1]} />
-      </div>
-      <div>
-        <Text>닉네임</Text>
-        <input type="text" />
-        <button>중복확인</button>
-      </div>
-      <div>
-        <Text>한마디</Text>
-        <textarea></textarea>
+        <div className="upload">
+          <div></div>
+          <input type="file" accept="image/*" />
+        </div>
+        <div>
+          <div>
+            <Text>닉네임</Text>
+            <input type="text" />
+            <button>중복확인</button>
+          </div>
+          <div>
+            <Text>한마디</Text>
+            <textarea></textarea>
+          </div>
+        </div>
       </div>
     </div>
   );
