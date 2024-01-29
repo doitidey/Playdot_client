d9c1b0cd11ba88287131c027e0c70c14d9af1605
doitidey <evxrafter@gmail.com>
2024-01-30 00:31:08 +0900

    Feature: 화면에 버튼 추가

--- src/components/signup/SignupSection.tsx
+++ src/components/signup/SignupSection.tsx
@@ -5,12 +5,33 @@ import "@/components/signup/SignupSection.scss";
 import useStore from "@/lib/store/signup/store";
 import SignupStepOne from "@/components/signup/SignupStepOne";
 import SignupStepTwo from "@/components/signup/SignupStepTwo";
+import SignupButtons from "./Buttons/SignupButtons";
 
 function SignupSection() {
-  const { signUpStep } = useStore();
+  const { signUpStep, increaseStep, decreaseStep } = useStore();
+
+  const handleOnclickNextButton = () => {
+    increaseStep();
+  };
+  const handleOnclickPreviousButton = () => {
+    decreaseStep();
+  };
 
   return (
     <section className="signup-block">
+      <div className="signup-block__buttons">
+        {signUpStep === 1 ? (
+          <SignupButtons title={"다음"} onClick={handleOnclickNextButton} />
+        ) : (
+          <>
+            <SignupButtons title={"다음"} onClick={handleOnclickNextButton} />
+            <SignupButtons
+              title={"이전"}
+              onClick={handleOnclickPreviousButton}
+            />
+          </>
+        )}
+      </div>
       <div className="signup-block__intro">
         <div className="signup-block__intro__circles">{signUpStep}</div>
       </div>
