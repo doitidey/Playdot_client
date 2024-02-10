2ed41d261f01505257c9612bbf4731a5026bf300
pildrums <there7788@gmail.com>
2024-02-10 16:28:12 +0900

    Refactor: Today 컴포넌트로 리팩토링 후 렌더링

--- src/app/match/today/page.tsx
+++ src/app/match/today/page.tsx
@@ -1,21 +1,12 @@
-import DateSection from "@/components/today/DateSection";
-import ScoreList from "@/components/today/ScoreList";
+import Today from "@/components/today/Today";
 import { Metadata } from "next";
-import "./TodayPage.scss";
-import Comment from "@/components/comment/Comment";
 
 export const metadata: Metadata = {
   title: "플레이닷 | 오늘의 승부예측",
 };
 
 function TodayPage() {
-  return (
-    <div className="today-page-block">
-      <DateSection />
-      <ScoreList />
-      <Comment />
-    </div>
-  );
+  return <Today />;
 }
 
 export default TodayPage;
