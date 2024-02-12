2739618e944469eeb60020f4ab4fe4eb7f6835f2
pildrums <there7788@gmail.com>
2024-02-12 16:46:17 +0900

    Refactor: 컴포넌트 내부의 함수 리팩토링 - getHomeLogo, getAwayLogo 함수 -> getLogo 파일에 리팩토링 - gameDate 함수 -> getGameTime 파일에 리팩토링

--- src/components/today/ScoreListItem.tsx
+++ src/components/today/ScoreListItem.tsx
@@ -1,61 +1,22 @@
 "use client";
 
-import { useCallback, useState } from "react";
+import { memo, useCallback, useState } from "react";
 import Text from "../common/Text";
 import "./ScoreListItem.scss";
 import classNames from "classnames";
 import Image from "next/image";
 import Title from "../common/Title";
 import { TodayMatchData } from "./Today";
-
-interface ScoreListItemProps extends TodayMatchData {}
+import { getAwayLogo, getHomeLogo } from "@/lib/util/getLogo";
+import { gameDate } from "@/lib/util/getGameTime";
 
 // Interface
-interface TeamImg {
-  lions: string;
-  twins: string;
-  landers: string;
-  bears: string;
-  tigers: string;
-  eagles: string;
-  heroes: string;
-  gients: string;
-  wiz: string;
-  dinos: string;
-}
-
-// Constants
-const TeamImgHref: TeamImg = {
-  lions: "/images/lions.svg",
-  tigers: "/images/tigers.svg",
-  twins: "/images/twins.svg",
-  bears: "/images/bears.svg",
-  gients: "/images/giants.svg",
-  dinos: "/images/dinos.svg",
-  landers: "/images/landers.svg",
-  wiz: "/images/wiz.svg",
-  eagles: "/images/eagles.svg",
-  heroes: "/images/heroes.svg",
-};
+interface ScoreListItemProps extends TodayMatchData {}
 
 // Component
 function ScoreListItem({ homeTeam, awayTeam, gameTime }: ScoreListItemProps) {
   const [selectHome, setSelectHome] = useState(false);
   const [selectAway, setSelectAway] = useState(false);
-
-  const gameDate = (data: string) => {
-    const rDataFormat = /[12][0-9]{3}-[0-9]{2}-[0-9]{2}/;
-    if (data.search(rDataFormat) === -1) {
-      return data;
-    }
-
-    const year = data.substring(0, 4);
-    const month = data.substring(5, 7);
-    const day = data.substring(10, 8);
-    const time = data.substring(11, 16);
-
-    return `${year}년 ${month}월 ${day}일 ${time}`;
-  };
 
   const onClickLeft = useCallback(() => {
     setSelectHome(!selectHome);
@@ -66,57 +27,6 @@ function ScoreListItem({ homeTeam, awayTeam, gameTime }: ScoreListItemProps) {
     setSelectAway(!selectAway);
     setSelectHome(false);
   }, [selectAway]);
-
-  // 리팩토링 예정 => 조건문이 너무 많음, 같은 로직의 함수가 중복 됨
-  const getHomeTeamSrc = (data: string) => {
-    if (homeTeam.teamName === "LG 트윈스") {
-      return TeamImgHref.twins;
-    } else if (homeTeam.teamName === "삼성 라이온즈") {
-      return TeamImgHref.lions;
-    } else if (homeTeam.teamName === "한화 이글스") {
-      return TeamImgHref.eagles;
-    } else if (homeTeam.teamName === "KIA 타이거즈") {
-      return TeamImgHref.tigers;
-    } else if (homeTeam.teamName === "키움 히어로즈") {
-      return TeamImgHref.heroes;
-    } else if (homeTeam.teamName === "롯데 자이언츠") {
-      return TeamImgHref.gients;
-    } else if (homeTeam.teamName === "SSG 랜더스") {
-      return TeamImgHref.landers;
-    } else if (homeTeam.teamName === "두산 베어스") {
-      return TeamImgHref.bears;
-    } else if (homeTeam.teamName === "KT 위즈") {
-      return TeamImgHref.wiz;
-    } else if (homeTeam.teamName === "NC 다이노스") {
-      return TeamImgHref.dinos;
-    }
-    return data;
-  };
-
-  const getAwayTeamSrc = (data: string) => {
-    if (awayTeam.teamName === "LG 트윈스") {
-      return TeamImgHref.twins;
-    } else if (awayTeam.teamName === "삼성 라이온즈") {
-      return TeamImgHref.lions;
-    } else if (awayTeam.teamName === "한화 이글스") {
-      return TeamImgHref.eagles;
-    } else if (awayTeam.teamName === "KIA 타이거즈") {
-      return TeamImgHref.tigers;
-    } else if (awayTeam.teamName === "키움 히어로즈") {
-      return TeamImgHref.heroes;
-    } else if (awayTeam.teamName === "롯데 자이언츠") {
-      return TeamImgHref.gients;
-    } else if (awayTeam.teamName === "SSG 랜더스") {
-      return TeamImgHref.landers;
-    } else if (awayTeam.teamName === "두산 베어스") {
-      return TeamImgHref.bears;
-    } else if (awayTeam.teamName === "KT 위즈") {
-      return TeamImgHref.wiz;
-    } else if (awayTeam.teamName === "NC 다이노스") {
-      return TeamImgHref.dinos;
-    }
-    return data;
-  };
 
   return (
     <>
@@ -132,7 +42,7 @@ function ScoreListItem({ homeTeam, awayTeam, gameTime }: ScoreListItemProps) {
         >
           <div className="team-img">
             <Image
-              src={getHomeTeamSrc(homeTeam.teamName)}
+              src={getHomeLogo(homeTeam.teamName)}
               alt="lions"
               width={0}
               height={0}
@@ -156,7 +66,7 @@ function ScoreListItem({ homeTeam, awayTeam, gameTime }: ScoreListItemProps) {
         >
           <div className="team-img">
             <Image
-              src={getAwayTeamSrc(awayTeam.teamName)}
+              src={getAwayLogo(awayTeam.teamName)}
               alt="lions"
               width={0}
               height={0}
@@ -177,4 +87,4 @@ function ScoreListItem({ homeTeam, awayTeam, gameTime }: ScoreListItemProps) {
   );
 }
 
-export default ScoreListItem;
+export default memo(ScoreListItem);
