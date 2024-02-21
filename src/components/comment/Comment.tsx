8093b973759de06371d0cc04a755cd7a348e104e
pildrums <there7788@gmail.com>
2024-02-21 20:59:30 +0900

    Fix: 댓글 입력 api 연동 작업 중

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -5,8 +5,8 @@ import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 import { MdRefresh } from "react-icons/md";
 import Title from "../common/Title";
 import CommentList from "./CommentList";
-import { useQuery } from "react-query";
-import { todayGamesComment } from "@/lib/api/todayAPI";
+import { useMutation, useQuery } from "react-query";
+import { postTodayComment, todayGamesComment } from "@/lib/api/todayAPI";
 
 export interface CommentData {
   profile: {
@@ -17,8 +17,8 @@ export interface CommentData {
   reply: {
     replyId?: number;
     content: string;
-    count: number;
-    createdAt: string;
+    count?: number;
+    createdAt?: string;
   };
 }
 
@@ -32,13 +32,19 @@ function Comment() {
   const [value, setValue] = useState("");
   const [comment, setComment] = useState<CommentData[]>([]);
 
-  const { data = comment } = useQuery({
-    queryKey: ["today-comment"],
+  const { data: commentData = comment } = useQuery({
+    queryKey: ["todayComment"],
     queryFn: () =>
       todayGamesComment()?.then((res) => {
         setComment(res.data.content);
+        console.log(res.data.content);
       }),
+    refetchOnWindowFocus: false,
   });
+
+  const mutation = useMutation("todayComment", async (comment: string) =>
+    postTodayComment(comment),
+  );
 
   const onChange = useCallback((event: ChangeEvent) => {
     event.preventDefault();
@@ -54,26 +60,24 @@ function Comment() {
         ...comment,
         {
           profile: {
-            nickname: "aaa",
-            teamName: "삼성 라이온즈",
+            teamName: localStorage.getItem("teamName") as string,
+            nickname: localStorage.getItem("nickname") as string,
           },
           reply: {
             content: value,
-            count: 0,
-            createdAt: "",
           },
         },
       ]);
       setValue("");
     },
-    [comment, value],
+    [value, comment],
   );
 
   return (
     <section className="comment-block">
       <form className="comment-block__content" onSubmit={onSubmit}>
         <div className="comment-header">
-          <Title medium>댓글 {data.length}개</Title>
+          <Title medium>댓글 {commentData.length}개</Title>
           <button type="submit">
             <MdRefresh />
           </button>
@@ -85,7 +89,7 @@ function Comment() {
           onChange={onChange}
           placeholder="댓글을 입력해주세요."
         />
-        <CommentList comment={data} />
+        <CommentList comment={commentData} />
       </form>
     </section>
   );
