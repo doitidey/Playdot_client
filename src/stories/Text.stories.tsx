4e6a7366d8b08e13eda74e7f41e20d365c8048d6
doitidey <evxrafter@gmail.com>
2024-03-04 13:13:13 +0900

    Docs: Text, Title 컴포넌트 스토리북 문서화 작업

--- src/stories/Text.stories.tsx
+++ src/stories/Text.stories.tsx
@@ -11,30 +11,67 @@ const meta: Meta<typeof Text> = {
     small: { control: true },
     caption: { control: true },
   },
+  parameters: {
+    layout: "centered",
+    componentSubtitle: "텍스트 출력 컴포넌트 입니다.",
+    docs: {
+      description: {
+        component: "크기 타입을 구분한 뒤, 알맞는 사이즈로 출력합니다.",
+      },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Text>;
 
 export const Large: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "18px / 500(medium)",
+      },
+    },
+  },
   args: {
     children: "Large",
     large: true,
   },
 };
 export const Medium: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "16px / 500(medium)",
+      },
+    },
+  },
   args: {
     children: "Medium",
     medium: true,
   },
 };
 export const Small: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "14px / 500(medium)",
+      },
+    },
+  },
   args: {
     children: "Small",
     small: true,
   },
 };
 export const Caption: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "12px / 400(normal)",
+      },
+    },
+  },
   args: {
     children: "Caption",
     caption: true,
--- src/stories/Title.stories.tsx
+++ src/stories/Title.stories.tsx
@@ -2,6 +2,8 @@ import { Meta, StoryObj } from "@storybook/react";
 import Title from "@/components/common/Title";
 
 const meta: Meta<typeof Title> = {
+  title: "TEXT/Tiltle",
+  tags: ["autodocs"],
   component: Title,
   argTypes: {
     largest: { control: true },
@@ -9,12 +11,28 @@ const meta: Meta<typeof Title> = {
     medium: { control: true },
     small: { control: true },
   },
+  parameters: {
+    layout: "centered",
+    componentSubtitle: "타이틀 출력 컴포넌트 입니다.",
+    docs: {
+      description: {
+        component: "크기 타입을 구분한 뒤, 알맞는 사이즈로 출력합니다.",
+      },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Title>;
 
 export const Largest: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "48px / 700(bold)",
+      },
+    },
+  },
   args: {
     children: "Largest",
     largest: true,
@@ -22,6 +40,13 @@ export const Largest: Story = {
 };
 
 export const Large: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "36px / 600(semi bold)",
+      },
+    },
+  },
   args: {
     children: "Large",
     large: true,
@@ -29,6 +54,13 @@ export const Large: Story = {
 };
 
 export const Medium: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "24px / 600(semi bold)",
+      },
+    },
+  },
   args: {
     children: "Medium",
     medium: true,
@@ -36,6 +68,13 @@ export const Medium: Story = {
 };
 
 export const Small: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: "22px / 500(medium)",
+      },
+    },
+  },
   args: {
     children: "Small",
     small: true,
