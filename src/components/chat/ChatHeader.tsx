06a5ba163081ed65149d2c1acb188cd6fb7137e6
doitidey <evxrafter@gmail.com>
2024-03-05 10:49:21 +0900

    Feature: Header 더미 데이터 연결 및 아이콘 추가

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -1,5 +1,7 @@
 import "@/components/chat/ChatHeader.scss";
 import Image from "next/image";
+import { GAME_DATA } from "./dummy";
+import { getTeamLogo } from "@/lib/util/TeamTagLogo";
 
 function ChatHeader() {
   return (
@@ -12,16 +14,34 @@ function ChatHeader() {
           height={30}
         />
       </div>
-      <div className="chat-header__score">3</div>
+      <div className="chat-header__score">{GAME_DATA.homeTeam.score}</div>
       <div className="chat-header__info">
-        <div className="date">23.11.23</div>
         <div className="teams">
-          <div className="teams__name">기아</div>
-          <div className="teams__vs">VS</div>
-          <div className="teams__name">키움</div>
+          <div className="teams__name">
+            <Image
+              src={getTeamLogo(GAME_DATA.homeTeam.teamName)}
+              alt={"hometeam logo"}
+              width={0}
+              height={41}
+            />
+            {GAME_DATA.homeTeam.teamName}
+          </div>
+          <div className="teams__desc">
+            <div className="date">{GAME_DATA.gameTime.split(" ")[0]}</div>
+            <div className="vs">VS</div>
+          </div>
+          <div className="teams__name">
+            <Image
+              src={getTeamLogo(GAME_DATA.awayTeam.teamName)}
+              alt={"awayTeam logo"}
+              width={0}
+              height={41}
+            />
+            {GAME_DATA.awayTeam.teamName}
+          </div>
         </div>
       </div>
-      <div className="chat-header__score">5</div>
+      <div className="chat-header__score">{GAME_DATA.awayTeam.score}</div>
       <div className="chat-header__button">
         <Image
           src={"/images/chatheaderarrow2.svg"}
