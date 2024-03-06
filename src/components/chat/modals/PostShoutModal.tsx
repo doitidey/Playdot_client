70317864f48987fa5f0087639412912331736ff1
doitidey <evxrafter@gmail.com>
2024-03-06 17:54:08 +0900

    Design: post shout modal 디자인

--- src/components/chat/modals/PostShoutModal.tsx
+++ src/components/chat/modals/PostShoutModal.tsx
@@ -1,11 +1,15 @@
 import TokenButton from "./TokenButton";
+import "@/components/chat/modals/PostShoutModal.scss";
 
 function PostShoutModal() {
   return (
-    <>
-      <input placeholder="외치기 내용을 입력해봐" />
+    <section className="post-shout">
+      <input
+        className="post-shout__input"
+        placeholder="외치기 내용을 입력해봐"
+      />
       <TokenButton />
-    </>
+    </section>
   );
 }
 export default PostShoutModal;
