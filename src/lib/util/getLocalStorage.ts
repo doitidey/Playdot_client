855503f98b7193b34241fd487e4f5d55003819ea
doitidey <evxrafter@gmail.com>
2024-03-07 13:35:47 +0900

    Feature: 프로필사진 저장 및 로컬 값을 읽는 변수 생성

---
+++ src/lib/util/getLocalStorage.ts
@@ -0,0 +1,7 @@
+export const getLocalNickname = localStorage.getItem("nickname");
+
+export const getLocalToken = localStorage.getItem("authToken");
+
+export const getLocalTeamName = localStorage.getItem("teamName");
+
+export const getLocalProfileImage = localStorage.getItem("profileImageUrl");
