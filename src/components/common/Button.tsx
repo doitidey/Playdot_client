32148ebe2260709d55739d4d7b59b2fbedb0d810
doitidey <evxrafter@gmail.com>
2024-03-07 21:25:39 +0900

    Design: x-small 사이즈 버튼 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,9 +7,9 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   /** 버튼 내용을 받아옵니다. */
   label?: string;
   /** 버튼 사이즈를 지정합니다. */
-  size: "small" | "medium" | "large";
+  size: "x-small" | "small" | "medium" | "large";
   /** 버튼 타입을 지정합니다. */
-  variant: "primary" | "secondary" | "active";
+  variant: "primary" | "disactive" | "active";
   /** 버튼 이벤트 함수를 받아옵니다. */
   onClick?: () => void;
 }
