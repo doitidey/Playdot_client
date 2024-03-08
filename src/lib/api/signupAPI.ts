96d7f8b25ce0e90a3354c5591b6913fcd8bcbe40
doitidey <evxrafter@gmail.com>
2024-03-08 16:36:37 +0900

    Refactor: 닉네임 체크 api 파라미터값 수정

--- src/lib/api/signupAPI.ts
+++ src/lib/api/signupAPI.ts
@@ -15,7 +15,7 @@ export const getTeams = () => {
 // GET
 export const nicknameCheck = (nickname: string) => {
   try {
-    const res = fetchData(`profile/nickname-check?${nickname}`, "get");
+    const res = fetchData(`profile/nickname-check?nickname=${nickname}`, "get");
     return res;
   } catch (error) {
     console.error(error);
