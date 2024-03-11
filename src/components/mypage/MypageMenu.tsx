c0b27c9c0fe08a9b8586f3c40a3cfa2006bab2bb
doitidey <evxrafter@gmail.com>
2024-03-11 16:56:42 +0900

    Design: 마이페이지 네비섹션 Ui 디벨롭

--- src/components/mypage/MypageMenu.tsx
+++ src/components/mypage/MypageMenu.tsx
@@ -1,13 +1,26 @@
+import Title from "../common/Title";
 import MypageFairyRecord from "./MypageFairyRecord";
 import MypagePresentRecord from "./MypagePresentRecord";
 import "@/components/mypage/MypageMenu.scss";
 
 function MypageMenu() {
   return (
-    <>
+    <div className="mypage-menu">
+      <div className="mypage-nav">
+        <div className="mypage-nav__item">
+          <Title medium className="mypage-nav__title ">
+            요정기록
+          </Title>
+        </div>
+        <div className="mypage-nav__item mypage-nav__item--active">
+          <Title medium className="mypage-nav__title mypage-nav__title--active">
+            선물내역
+          </Title>
+        </div>
+      </div>
       <MypageFairyRecord />
       <MypagePresentRecord />
-    </>
+    </div>
   );
 }
 
