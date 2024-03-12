459e13ff2033a5c5a80631f9e80129bab6d055af
doitidey <evxrafter@gmail.com>
2024-03-12 13:33:01 +0900

    Design: 요정기록 컴포넌트 ui 구현

--- src/components/mypage/MypageFairyRecord.tsx
+++ src/components/mypage/MypageFairyRecord.tsx
@@ -1,10 +1,67 @@
 import "@/components/mypage/MypageFairyRecord.scss";
+import Title from "../common/Title";
+import Image from "next/image";
 
 function MypageFairyRecord() {
   return (
-    <>
-      <p>요정기록</p>
-    </>
+    <div className="fairy">
+      <div className="fairy__content">
+        <Title large className="fairy__title">
+          승리요정
+        </Title>
+        <div className="medal">
+          <Image
+            className="medal__confeti"
+            src={"images/confetti.svg"}
+            alt={"컨페티"}
+            width={294}
+            height={132}
+          />
+          <Image
+            className="medal__medalbg"
+            src={"images/fairypolygon.svg"}
+            alt={"메달"}
+            width={380}
+            height={400}
+          />
+          <Image
+            className="medal__number"
+            src={"images/3.svg"}
+            alt={"넘버"}
+            width={80}
+            height={92}
+          />
+        </div>
+        <p>총 3회 달성</p>
+      </div>
+      <div className="fairy__content">
+        <Title large> 패배요정 </Title>
+        <div className="medal">
+          <Image
+            className="medal__confeti"
+            src={"images/confetti.svg"}
+            alt={"컨페티"}
+            width={294}
+            height={132}
+          />
+          <Image
+            className="medal__number"
+            src={"images/3.svg"}
+            alt={"넘버"}
+            width={80}
+            height={92}
+          />
+          <Image
+            className="medal__medalbg"
+            src={"images/fairypolygon.svg"}
+            alt={"메달"}
+            width={380}
+            height={400}
+          />
+        </div>
+        <p>총 3회 달성</p>
+      </div>
+    </div>
   );
 }
 
