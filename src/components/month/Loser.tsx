a3a9313eb83a29b5aa73393cc4d029c8ea5fdea5
pildrums <there7788@gmail.com>
2024-03-14 19:21:29 +0900

    Fix: 월간 승리요정 UI 정리 - 현재 UI 레이아웃 수정 필요(반응형 페이지 대응 준비) - 기본 레이아웃 완료

--- src/components/month/Loser.tsx
+++ src/components/month/Loser.tsx
@@ -18,26 +18,26 @@ function Loser() {
           <Title large>닉네임</Title>
           <div className="last-floor">
             <div className="last-floor__content">
-              <Text>승리요정 1회 / 패배요정 2회</Text>
-              <Text>&quot;내가 이겼지?&quot;</Text>
+              <Text small>승리요정 1회 / 패배요정 2회</Text>
+              <Text large>&quot;내가 이겼지?&quot;</Text>
             </div>
             <Image src="/images/lions.svg" width={60} height={60} alt="" />
           </div>
         </li>
-        <li className="month-list__item">
-          <Text>이등은 아이디만 보임</Text>
+        <li className="month-list__second">
+          <Text large>이등은 아이디만 보임</Text>
           <Image src="/images/lions.svg" width={60} height={60} alt="" />
         </li>
-        <li className="month-list__item">
-          <Text>이등은 아이디만 보임</Text>
+        <li className="month-list__second">
+          <Text large>아이디!!!!</Text>
           <Image src="/images/lions.svg" width={60} height={60} alt="" />
         </li>
-        <li className="month-list__item">
-          <Text>이등은 아이디만 보임</Text>
+        <li className="month-list__second">
+          <Text large>아아아아이디!!!</Text>
           <Image src="/images/lions.svg" width={60} height={60} alt="" />
         </li>
-        <li className="month-list__item">
-          <Text>이등은 아이디만 보임</Text>
+        <li className="month-list__second">
+          <Text large>으아아아아이디!!</Text>
           <Image src="/images/lions.svg" width={60} height={60} alt="" />
         </li>
       </ul>
