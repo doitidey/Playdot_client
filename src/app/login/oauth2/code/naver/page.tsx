ae88dd40e356f1e539e847635564e5433127c0ab
doitidey <evxrafter@gmail.com>
2024-03-19 11:20:50 +0900

    Feature: 구글, 네이버 페이지 생성

---
+++ src/app/login/oauth2/code/naver/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { oauthLogin } from "@/lib/api/authAPI";
+import { useEffect } from "react";
+
+function Oauth() {
+  const code = window.location.search;
+  useEffect(() => {
+    try {
+      const res = oauthLogin("naver", code);
+      console.log(code);
+      console.log(res);
+    } catch (error) {
+      console.error("로그인 실패:", error);
+    }
+  }, []);
+
+  return (
+    <>
+      <div>로그인</div>
+    </>
+  );
+}
+
+export default Oauth;
