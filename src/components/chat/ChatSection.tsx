b0179150a7c22b8dff64d1ff6855400fd3f696f4
doitidey <evxrafter@gmail.com>
2024-03-30 14:11:56 +0900

    Feature: send 추가 및 주석 제거

--- src/components/chat/ChatSection.tsx
+++ src/components/chat/ChatSection.tsx
@@ -2,66 +2,75 @@
 
 import "@/components/chat/ChatSection.scss";
 
+import { useEffect, useState } from "react";
+import { Client } from "@stomp/stompjs";
+
 import ChatHeader from "@/components/chat/ChatHeader";
 import ChatLog from "@/components/chat/chatlog/ChatLog";
 import MenuModal from "@/components/chat/modals/MenuModal";
 import ChatInput from "@/components/chat/ChatInput";
 import useMenuModalState from "@/lib/store/chat/menuModalState";
 import VoteModal from "@/components/chat/modals/VoteModal";
-import ShoutBubble from "./chatlog/ShoutBubble";
-import { useEffect, useState } from "react";
-// import { Stomp } from "@stomp/stompjs";
-// import SockJS from "sockjs-client";
+import ShoutBubble from "@/components/chat/chatlog/ShoutBubble";
 import { configureStompClient } from "@/lib/api/chatAPI";
 
 function ChatSection() {
+  const ROOMNUM = 1; //TODO: NextJS url praram
+
   const { menuModalState } = useMenuModalState();
   const [showShoutBubble, setShowShoutBubble] = useState(true);
+  const [stompClient, setStompClient] = useState<Client | null>(null);
 
   useEffect(() => {
-    // const connect = () => {
-    //   const token = localStorage.getItem("authToken");
-    //   const serverUrl = `${process.env.NEXT_PUBLIC_BASE_URL}chat`;
-    //   const sock = new SockJS(serverUrl);
-    //   const stompClient = Stomp.over(sock);
+    const socket = configureStompClient(ROOMNUM);
 
-    //   stompClient.connect({ Authorization: `${token}`, gameId: `1` }, () => {
-    //     stompClient.subscribe(`/sub/chat/room/1`, (message) => {
-    //       try {
-    //         const receivedMessage = JSON.parse(message.body);
-    //         console.log(receivedMessage);
-    //       } catch (error) {
-    //         console.error("Error parsing message", error);
-    //       }
-    //     });
-    //   });
-
-    const socket = configureStompClient(1);
-    socket.onConnect = function (frame) {
+    socket.onConnect = (frame: frame) => {
       console.log("connected", frame);
-      socket.subscribe(`/sub/chat/room/1`, (message) => {
-        try {
-          const receivedMessage = JSON.parse(message.body);
-          console.log(receivedMessage);
-        } catch (error) {
-          console.error("Error parsing message", error);
-        }
-      });
+      setStompClient(socket);
+      socket.subscribe(
+        `/sub/chat/room/${ROOMNUM}`,
+        (frame) => {
+          try {
+            console.log("구독요청");
+            // const receivedMessage = JSON.parse(frame.body);
+            console.log(frame);
+          } catch (error) {
+            console.error("stomp 구독에 오류가 발생했습니다:", error);
+          }
+        },
+        {
+          gameId: `${ROOMNUM}`,
+          Authorization: `${localStorage.getItem("authToken")}`,
+        },
+      );
     };
 
-    socket.onStompError = function (frame) {
+    socket.onStompError = (frame) => {
       console.log("Broker reported error: " + frame.headers["message"]);
       console.log("Additional details: " + frame.body);
     };
 
     socket.activate();
-
-    return () => {
-      if (socket) {
-        socket.deactivate();
-      }
-    };
   }, []);
+
+  const sendMessage = (msg: any) => {
+    if (stompClient && stompClient.connected) {
+      const messageDetails = JSON.stringify({
+        gameId: `${ROOMNUM}`,
+        type: "NORMAL",
+        content: msg,
+      });
+
+      stompClient.publish({
+        destination: "/pub/chat/message",
+        body: messageDetails,
+        headers: {
+          gameId: `${ROOMNUM}`,
+          Authorization: `${localStorage.getItem("authToken")}`,
+        },
+      });
+    }
+  };
 
   return (
     <div className="chat">
@@ -72,7 +81,7 @@ function ChatSection() {
             {menuModalState.isOpen && <MenuModal />}
             <VoteModal />
           </div>
-          <ChatInput />
+          <ChatInput sendMessage={sendMessage} />
         </div>
         <ChatLog />
       </div>
--- src/lib/api/chatAPI.ts
+++ src/lib/api/chatAPI.ts
@@ -1,19 +1,20 @@
-import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
 
 export const configureStompClient = (gameId: number) => {
   const serverUrl = `${process.env.NEXT_PUBLIC_BASE_URL}chat`;
   const token = localStorage.getItem("authToken");
-  console.log(token);
   const sockJs = new SockJS(serverUrl);
   const stompClient = new Client({
-    brokerURL: serverUrl,
     webSocketFactory: () => sockJs,
     connectHeaders: {
       gameId: `${gameId}`,
       Authorization: `${token}`,
     },
-    debug: function (str) {
+    // onStompError: (frame) => {
+    //   console.log("ㄷㄱ객", frame);
+    // },
+    debug: (str) => {
       console.log(str);
     },
     reconnectDelay: 5000,
