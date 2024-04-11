e3aeaf1363d14d75a56d5cea088dca2c537ec22f
doitidey <evxrafter@gmail.com>
2024-04-11 14:59:22 +0900

    Feature: 외치기 전송 api 연결

--- src/components/chat/modals/PostShoutModal.tsx
+++ src/components/chat/modals/PostShoutModal.tsx
@@ -1,16 +1,35 @@
 import Button from "@/components/common/Button";
 import TokenButton from "./TokenButton";
 import "@/components/chat/modals/PostShoutModal.scss";
+import { useSocket } from "@/lib/hooks/useSocket";
+import { useState } from "react";
 
 function PostShoutModal() {
+  const { sendMessage } = useSocket();
+  const [message, setMessage] = useState("");
+
+  const onClickShout = () => {
+    const messageDetail = {
+      message: message,
+      type: "BAWWLING",
+    };
+    sendMessage(messageDetail);
+  };
+
   return (
     <section className="post-shout">
       <input
         className="post-shout__input"
         placeholder="외치기 내용을 입력해봐"
+        onChange={(e) => setMessage(e.target.value)}
       />
       <TokenButton />
-      <Button label="토큰 5개 사용" size="large" variant="active" />
+      <Button
+        label="토큰 5개 사용"
+        size="large"
+        variant={`${message ? "active" : "disactive"}`}
+        onClick={onClickShout}
+      />
     </section>
   );
 }
