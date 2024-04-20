17d19e1063aae4641360e44a5ece446b085deb1b
doitidey <evxrafter@gmail.com>
2024-04-20 14:41:07 +0900

    Feature: 네이버 구글 소셜로그인 연결

--- src/app/login/oauth2/code/google/page.tsx
+++ src/app/login/oauth2/code/google/page.tsx
@@ -1,22 +1,41 @@
 "use client";
 import { oauthLogin } from "@/lib/api/authAPI";
+import { useUserDataStore } from "@/lib/store/userDataStore";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 function Oauth() {
   const code = window.location.search;
+  const router = useRouter();
+
+  const { userData, setUserData } = useUserDataStore();
   useEffect(() => {
-    try {
-      const res = oauthLogin("google", code);
-      console.log(code);
-      console.log(res);
-    } catch (error) {
-      console.error("로그인 실패:", error);
-    }
+    const getOathLogin = async () => {
+      try {
+        const res = await oauthLogin("google", code);
+        const userProfile = {
+          profileImageUrl: res.data.profileImageUrl,
+          nickname: res.data.nickname,
+          teamName: res.data.teamName,
+          newMember: res.data.newMember,
+        };
+        setUserData(userProfile);
+      } catch (error) {
+        console.error("로그인 실패:", error);
+      } finally {
+        if (!userData.newMember && userData.teamName) {
+          router.push("/signup");
+        } else {
+          router.push("/match/today");
+        }
+      }
+    };
+    getOathLogin();
   }, []);
 
   return (
     <>
-      <div>로그인</div>
+      <div>로그인중...</div>
     </>
   );
 }
--- src/lib/util/login/loginButton.ts
+++ src/lib/util/login/loginButton.ts
@@ -11,7 +11,7 @@ export const BUTTON_INFO: ButtonInfo = [
 
 type LoginLinks = { [naver: string]: string; kakao: string; google: string };
 export const LOGIN_LINKS: LoginLinks = {
-  naver: ``,
+  naver: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_REST_API_KEY}&state=playdot&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI}`,
   kakao: `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`,
   google: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email profile`,
 };
