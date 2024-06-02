39873d0565410fa4881b703b232ac53fc4f6ea0c
doitidey <evxrafter@gmail.com>
2024-06-02 15:35:29 +0900

    Feature: 토큰값 조회api 연결

--- src/components/chat/modals/TokenButton.tsx
+++ src/components/chat/modals/TokenButton.tsx
@@ -1,11 +1,26 @@
 import "@/components/chat/modals/TokenButton.scss";
+import { getProfileDetails } from "@/lib/api/mypageAPI";
+import { useEffect, useState } from "react";
 
 //todo: token값 api값 연동
 function TokenButton() {
+  const [token, setToken] = useState<number>(0);
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const res = await getProfileDetails();
+        setToken(res.data.token);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getToken();
+  }, []);
+
   return (
     <div className="token__block">
       <p>💰 내 토큰</p>
-      <p>3tk</p>
+      <p>{token}tk</p>
     </div>
   );
 }
