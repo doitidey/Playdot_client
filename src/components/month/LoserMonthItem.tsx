3f3a17dd40b4abd906115820ed696b320bccc16c
pildrums <there7788@gmail.com>
2024-06-13 18:04:03 +0900

    Feature: 월간 승리요정 데이터 바인딩 및 렌더링 완료 - 글자수 제한 구현 완료 (닉네임) - 등수에 따른 등수 아이콘 변경 함수 구현 완료

--- src/components/month/LoserMonthItem.tsx
+++ src/components/month/LoserMonthItem.tsx
@@ -2,8 +2,28 @@ import "@/components/month/MonthItem.scss";
 import Title from "../common/Title";
 import Text from "../common/Text";
 import Image from "next/image";
+import { MonthData } from "./Month";
 
-function LoserMonthItem() {
+interface LoserMonthItemProps {
+  monthData: MonthData;
+}
+
+function LoserMonthItem({ monthData }: LoserMonthItemProps) {
+  const truncateNickname = (str: string, n: number) => {
+    return str && str.length > n ? str.substring(0, n - 1) + "..." : str;
+  };
+  const renderRankImg = (rank: number) => {
+    switch (rank) {
+      case (rank = 2):
+        return "/images/second.svg";
+      case (rank = 3):
+        return "/images/third.svg";
+      case (rank = 4):
+        return "/images/fourth.svg";
+      case (rank = 5):
+        return "/images/fourth.svg";
+    }
+  };
   return (
     <>
       <div className="block">
@@ -22,81 +42,40 @@ function LoserMonthItem() {
           {/* 추후 Image 컴포넌트로 변경 */}
           <div className="image-percentage">
             <div className="user-image" />
-            <Title largest>90%</Title>
+            <Title largest>{monthData?.loseMembers[0].voteRatio}%</Title>
           </div>
           <div className="text-logo">
             <div className="text">
-              <Title large>닉네임</Title>
-              <Text large>승리요정 1회 / 패배요정 2회</Text>
-              <Title medium>승리요정 1회 / 패배요정 2회</Title>
+              <Title large>
+                {truncateNickname(monthData?.loseMembers[0].nickname, 5)}
+              </Title>
+              <Text large>
+                승리요정 {monthData?.loseMembers[0].winFairyCount}회 / 패배요정{" "}
+                {monthData?.loseMembers[0].loseFairyCount}회
+              </Text>
+              <Title medium>{monthData?.loseMembers[0].title}</Title>
             </div>
             <Image src="/images/lions.svg" alt="" width={60} height={60} />
           </div>
         </div>
-        <div className="secondary">
-          <div className="icon-container">
-            <Image
-              className="first-icon"
-              src="/images/second.svg"
-              alt=""
-              width={100}
-              height={100}
-            />
-            <Text large>2nd</Text>
+        {monthData?.loseMembers.slice(1, 5).map((item) => (
+          <div key={item.memberId} className="secondary">
+            <div className="icon-container">
+              <Image
+                className="first-icon"
+                src={renderRankImg(item.rank) as string}
+                alt=""
+                width={100}
+                height={100}
+              />
+              <Text large>{item.rank}nd</Text>
+            </div>
+            <div className="user-icon">
+              <Title small>{truncateNickname(item.nickname, 6)}</Title>
+              <Image src="/images/lions.svg" alt="" width={60} height={60} />
+            </div>
           </div>
-          <div className="user-icon">
-            <Title small>닉네임</Title>
-            <Image src="/images/lions.svg" alt="" width={60} height={60} />
-          </div>
-        </div>
-        <div className="secondary">
-          <div className="icon-container">
-            <Image
-              className="first-icon"
-              src="/images/third.svg"
-              alt=""
-              width={100}
-              height={100}
-            />
-            <Text large>3rd</Text>
-          </div>
-          <div className="user-icon">
-            <Title small>닉네임</Title>
-            <Image src="/images/lions.svg" alt="" width={60} height={60} />
-          </div>
-        </div>
-        <div className="secondary">
-          <div className="icon-container">
-            <Image
-              className="first-icon"
-              src="/images/fourth.svg"
-              alt=""
-              width={100}
-              height={100}
-            />
-            <Text large>4th</Text>
-          </div>
-          <div className="user-icon">
-            <Title small>닉네임</Title>
-            <Image src="/images/lions.svg" alt="" width={60} height={60} />
-          </div>
-        </div>
-        <div className="secondary">
-          <div className="icon-container">
-            <Image
-              className="first-icon"
-              src="/images/fourth.svg"
-              alt=""
-              width={100}
-              height={100}
-            />
-            <Text large>5th</Text>
-          </div>
-          <div className="user-icon">
-            <Title small>닉네임</Title>
-            <Image src="/images/lions.svg" alt="" width={60} height={60} />
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
