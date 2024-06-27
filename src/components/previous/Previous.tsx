16669331eb887d3da7b84ab774d50529762db998
pildrums <there7788@gmail.com>
2024-06-28 07:46:33 +0900

    Fix: useQueries로 데이터 병렬로 받아오게 처리

--- src/components/previous/Previous.tsx
+++ src/components/previous/Previous.tsx
@@ -4,11 +4,20 @@ import "@/components/previous/Previous.scss";
 import Select from "@/components/previous/Select";
 import Title from "../common/Title";
 import TeamInfo from "./TeamInfo";
-
-// interface SelectArray {
-//   title: string;
-//   text: string;
-// }
+import { useQueries } from "react-query";
+import { getPrevious } from "@/lib/api/previousAPI";
+import {
+  fifthEndDate,
+  fifthStartDate,
+  firstEndDate,
+  firstStartDate,
+  fourthEndDate,
+  fourthStartDate,
+  secondEndDate,
+  secondStartDate,
+  thirdEndDate,
+  thirdStartDate,
+} from "@/lib/util/previous/date";
 
 export interface PreviousData {
   gameId: number;
@@ -26,30 +35,42 @@ export interface PreviousData {
   voteTeamId: number;
 }
 
-// const selectArr: SelectArray[] = [
-//   {
-//     title: "1주",
-//     text: "12.1 - 12.7",
-//   },
-//   {
-//     title: "2주",
-//     text: "12.8 - 12.14",
-//   },
-//   {
-//     title: "3주",
-//     text: "12.15 - 12.21",
-//   },
-//   {
-//     title: "4주",
-//     text: "12.22 - 12.28",
-//   },
-//   {
-//     title: "5주",
-//     text: "12.29 - 12.31",
-//   },
-// ];
-
 function Previous() {
+  const previousData = useQueries<PreviousData[]>([
+    {
+      queryKey: ["previous", 1],
+      queryFn: () => getPrevious(firstStartDate, firstEndDate),
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+    {
+      queryKey: ["previous", 2],
+      queryFn: () => getPrevious(secondStartDate, secondEndDate),
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+    {
+      queryKey: ["previous", 3],
+      queryFn: () => getPrevious(thirdStartDate, thirdEndDate),
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+    {
+      queryKey: ["previous", 4],
+      queryFn: () => getPrevious(fourthStartDate, fourthEndDate),
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+    {
+      queryKey: ["previous", 5],
+      queryFn: () => getPrevious(fifthStartDate, fifthEndDate),
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+  ]);
+
+  console.warn(previousData);
+
   return (
     <section className="previous-block">
       <div className="previous-block__content">
