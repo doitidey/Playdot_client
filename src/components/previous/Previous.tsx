e1d0a6dc4bebd1f46665febb9e3bf27557eaa261
pildrums <there7788@gmail.com>
2024-06-28 15:03:11 +0900

    Feature: API 호출 함수 작성 - useQuery로 각 주별 API 호출 함수 구현 - 추후 useQueries로 병렬 호출 구현 예정

--- src/components/previous/Previous.tsx
+++ src/components/previous/Previous.tsx
@@ -4,7 +4,7 @@ import "@/components/previous/Previous.scss";
 import Select from "@/components/previous/Select";
 import Title from "../common/Title";
 import TeamInfo from "./TeamInfo";
-import { useQueries } from "react-query";
+import { useQuery } from "react-query";
 import { getPrevious } from "@/lib/api/previousAPI";
 import {
   fifthEndDate,
@@ -18,65 +18,73 @@ import {
   thirdEndDate,
   thirdStartDate,
 } from "@/lib/util/previous/date";
-
-export interface PreviousData {
-  gameId: number;
-  homeTeam: {
-    teamName: string;
-    voteRatio: number;
-    id: number;
-  };
-  awayTeam: {
-    teamName: string;
-    voteRatio: number;
-    id: number;
-  };
-  gameTime: string;
-  voteTeamId: number;
-}
+import { useState } from "react";
+import { PreviousData } from "@/lib/types/previous/previous";
 
 function Previous() {
-  const previousData = useQueries<PreviousData[]>([
-    {
-      queryKey: ["previous", 1],
-      queryFn: () => getPrevious(firstStartDate, firstEndDate),
-      staleTime: 1000 * 60,
-      cacheTime: 1000 * 60 * 5,
-    },
-    {
-      queryKey: ["previous", 2],
-      queryFn: () => getPrevious(secondStartDate, secondEndDate),
-      staleTime: 1000 * 60,
-      cacheTime: 1000 * 60 * 5,
-    },
-    {
-      queryKey: ["previous", 3],
-      queryFn: () => getPrevious(thirdStartDate, thirdEndDate),
-      staleTime: 1000 * 60,
-      cacheTime: 1000 * 60 * 5,
-    },
-    {
-      queryKey: ["previous", 4],
-      queryFn: () => getPrevious(fourthStartDate, fourthEndDate),
-      staleTime: 1000 * 60,
-      cacheTime: 1000 * 60 * 5,
-    },
-    {
-      queryKey: ["previous", 5],
-      queryFn: () => getPrevious(fifthStartDate, fifthEndDate),
-      staleTime: 1000 * 60,
-      cacheTime: 1000 * 60 * 5,
-    },
-  ]);
+  // Hooks
+  const [activeTab, setActiveTab] = useState(0);
 
-  console.warn(previousData);
+  // useQuery
+  const { data: firstData } = useQuery<PreviousData[]>(
+    ["previous", 0],
+    () => getPrevious(firstStartDate, firstEndDate),
+    {
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+  );
+  const { data: secondData } = useQuery<PreviousData[]>(
+    ["previous", 1],
+    () => getPrevious(secondStartDate, secondEndDate),
+    {
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+  );
+  const { data: thirdData } = useQuery<PreviousData[]>(
+    ["previous", 2],
+    () => getPrevious(thirdStartDate, thirdEndDate),
+    {
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+  );
+  const { data: fourthData } = useQuery<PreviousData[]>(
+    ["previous", 3],
+    () => getPrevious(fourthStartDate, fourthEndDate),
+    {
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+  );
+  const { data: fifthData } = useQuery<PreviousData[]>(
+    ["previous", 4],
+    () => getPrevious(fifthStartDate, fifthEndDate),
+    {
+      staleTime: 1000 * 60,
+      cacheTime: 1000 * 60 * 5,
+    },
+  );
+
+  // event function
+  const onSelectClick = (index: number) => {
+    setActiveTab(index);
+  };
 
   return (
     <section className="previous-block">
       <div className="previous-block__content">
         <Title medium>지난 예측</Title>
-        <Select />
-        <TeamInfo />
+        <Select activeTab={activeTab} onSelectClick={onSelectClick} />
+        <TeamInfo
+          activeTab={activeTab}
+          firstData={firstData as PreviousData[]}
+          secondData={secondData as PreviousData[]}
+          thirdData={thirdData as PreviousData[]}
+          fourthData={fourthData as PreviousData[]}
+          fifthData={fifthData as PreviousData[]}
+        />
       </div>
     </section>
   );
