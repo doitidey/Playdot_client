b156f7e4085d36a0cb6e726aff2454cf16d29a1c
pildrums <there7788@gmail.com>
2024-06-28 15:05:40 +0900

    Fix: selectArr, SelectProps interface 분리

--- src/components/previous/Select.tsx
+++ src/components/previous/Select.tsx
@@ -1,62 +1,19 @@
 import "@/components/previous/Select.scss";
 import Text from "../common/Text";
 import Title from "../common/Title";
-import { useState } from "react";
-import {
-  fifthEndDateSub,
-  fifthStartDateSub,
-  firstEndDateSub,
-  firstStartDateSub,
-  fourthEndDateSub,
-  fourthStartDateSub,
-  secondEndDateSub,
-  secondStartDateSub,
-  thirdEndDateSub,
-  thirdStartDateSub,
-} from "@/lib/util/previous/date";
+import { selectArr } from "@/lib/util/previous/select";
+import { SelectProps } from "@/lib/types/previous/select";
 
-interface SelectArray {
-  title: string;
-  text: string;
-}
-
-const selectArr: SelectArray[] = [
-  {
-    title: "1주",
-    text: `${firstStartDateSub} ~ ${firstEndDateSub}`,
-  },
-  {
-    title: "2주",
-    text: `${secondStartDateSub} ~ ${secondEndDateSub}`,
-  },
-  {
-    title: "3주",
-    text: `${thirdStartDateSub} ~ ${thirdEndDateSub}`,
-  },
-  {
-    title: "4주",
-    text: `${fourthStartDateSub} ~ ${fourthEndDateSub}`,
-  },
-  {
-    title: "5주",
-    text: `${fifthStartDateSub} ~ ${fifthEndDateSub}`,
-  },
-];
-
-function Select() {
-  const [activeTab, setActiveTab] = useState(0);
-  const onSelectClick = (index: number) => {
-    setActiveTab(index);
-  };
+function Select({ activeTab, onSelectClick }: SelectProps) {
   return (
     <ul className="select-block">
-      {selectArr.map((item, index) => (
+      {selectArr.map((item) => (
         <li
-          key={index}
+          key={item.id}
           className={`select-block__item ${
-            activeTab === index ? "active-select" : ""
+            activeTab === item.id ? "active-select" : ""
           }`}
-          onClick={() => onSelectClick(index)}
+          onClick={() => onSelectClick(item.id)}
         >
           <Title small>{item.title}</Title>
           <Text small>{item.text}</Text>
