2fa1403e2a9db596e8ef1d0b5a1695c4df185584
pildrums <there7788@gmail.com>
2024-07-19 15:09:22 +0900

    Fix: 날짜별 5경기 나누는 작업 완료

--- src/components/previous/TeamInfo.tsx
+++ src/components/previous/TeamInfo.tsx
@@ -3,6 +3,7 @@
 import "@/components/previous/TeamInfo.scss";
 import TeamInfoItem from "./TeamInfoItem";
 import { PreviousData } from "@/lib/types/previous/previous";
+import Text from "../common/Text";
 
 interface TeamInfoProps {
   activeTab: number;
@@ -21,103 +22,430 @@ function TeamInfo({
   fourthData,
   fifthData,
 }: TeamInfoProps) {
+  console.warn(fourthData);
   switch (activeTab) {
     case 0: {
       return (
         <ul className="team-info-block">
+          <Text medium>
+            {firstData && firstData[0]?.games?.find((item) => item)?.gameDate}
+          </Text>
           {firstData &&
-            firstData.map((item) =>
-              item.games.map((item, index) => (
-                <>
-                  <TeamInfoItem
-                    key={index}
-                    awayTeam={item.awayTeam}
-                    homeTeam={item.homeTeam}
-                    voteTeamId={item.voteTeamId}
-                  />
-                </>
-              )),
-            )}
+            firstData[0]?.games?.map((item) => (
+              <>
+                <TeamInfoItem
+                  key={item.gameId}
+                  awayTeam={item.awayTeam}
+                  homeTeam={item.homeTeam}
+                  voteTeamId={item.voteTeamId}
+                />
+              </>
+            ))}
+          <hr />
+          <Text medium>
+            {firstData && firstData[1]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {firstData &&
+            firstData[1]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {firstData && firstData[2]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {firstData &&
+            firstData[2].games.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {firstData && firstData[3]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {firstData &&
+            firstData[3]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {firstData && firstData[4]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {firstData &&
+            firstData[4]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {firstData && firstData[5]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {firstData &&
+            firstData[5]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
         </ul>
       );
     }
     case 1: {
       return (
         <ul className="team-info-block">
+          <Text medium>
+            {secondData && secondData[0]?.games?.find((item) => item)?.gameDate}
+          </Text>
           {secondData &&
-            secondData.map((item) =>
-              item.games.map((item) => (
-                <>
-                  <TeamInfoItem
-                    key={item.gameId}
-                    awayTeam={item.awayTeam}
-                    homeTeam={item.homeTeam}
-                    gameDate={item.gameDate}
-                    voteTeamId={item.voteTeamId}
-                  />
-                </>
-              )),
-            )}
+            secondData[0]?.games?.map((item) => (
+              <>
+                <TeamInfoItem
+                  key={item.gameId}
+                  awayTeam={item.awayTeam}
+                  homeTeam={item.homeTeam}
+                  voteTeamId={item.voteTeamId}
+                />
+              </>
+            ))}
+          <hr />
+          <Text medium>
+            {secondData && secondData[1]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {secondData &&
+            secondData[1]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {secondData && secondData[2]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {secondData &&
+            secondData[2]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {secondData && secondData[3]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {secondData &&
+            secondData[3]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {secondData && secondData[4]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {secondData &&
+            secondData[4]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {secondData && secondData[5]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {secondData &&
+            secondData[5]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
         </ul>
       );
     }
     case 2: {
       return (
         <ul className="team-info-block">
+          <Text medium>
+            {thirdData && thirdData[0]?.games?.find((item) => item)?.gameDate}
+          </Text>
           {thirdData &&
-            thirdData.map((item) =>
-              item.games.map((item) => (
-                <>
-                  <TeamInfoItem
-                    key={item.gameId}
-                    awayTeam={item.awayTeam}
-                    homeTeam={item.homeTeam}
-                    gameDate={item.gameDate}
-                    voteTeamId={item.voteTeamId}
-                  />
-                </>
-              )),
-            )}
+            thirdData[0]?.games?.map((item) => (
+              <>
+                <TeamInfoItem
+                  key={item.gameId}
+                  awayTeam={item.awayTeam}
+                  homeTeam={item.homeTeam}
+                  voteTeamId={item.voteTeamId}
+                />
+              </>
+            ))}
+          <hr />
+          <Text medium>
+            {thirdData && thirdData[1]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {thirdData &&
+            thirdData[1]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {thirdData && thirdData[2]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {thirdData &&
+            thirdData[2]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {thirdData && thirdData[3]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {thirdData &&
+            thirdData[3]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {thirdData && thirdData[4]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {thirdData &&
+            thirdData[4]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {thirdData && thirdData[5]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {thirdData &&
+            thirdData[5]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
         </ul>
       );
     }
     case 3: {
       return (
         <ul className="team-info-block">
+          <Text medium>
+            {fourthData && fourthData[0]?.games?.find((item) => item)?.gameDate}
+          </Text>
           {fourthData &&
-            fourthData.map((item) =>
-              item.games.map((item) => (
-                <>
-                  <TeamInfoItem
-                    key={item.gameId}
-                    awayTeam={item.awayTeam}
-                    homeTeam={item.homeTeam}
-                    gameDate={item.gameDate}
-                    voteTeamId={item.voteTeamId}
-                  />
-                </>
-              )),
-            )}
+            fourthData[0]?.games?.map((item) => (
+              <>
+                <TeamInfoItem
+                  key={item.gameId}
+                  awayTeam={item.awayTeam}
+                  homeTeam={item.homeTeam}
+                  voteTeamId={item.voteTeamId}
+                />
+              </>
+            ))}
+          <hr />
+          <Text medium>
+            {fourthData && fourthData[1]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fourthData &&
+            fourthData[1]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {fourthData && fourthData[2]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fourthData &&
+            fourthData[2]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {fourthData && fourthData[3]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fourthData &&
+            fourthData[3]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {fourthData && fourthData[4]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fourthData &&
+            fourthData[4]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {fourthData && fourthData[5]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fourthData &&
+            fourthData[5]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
         </ul>
       );
     }
     case 4: {
       return (
         <ul className="team-info-block">
+          <Text medium>
+            {fifthData && fifthData[0]?.games?.find((item) => item)?.gameDate}
+          </Text>
           {fifthData &&
-            fifthData.map((item) =>
-              item.games.map((item) => (
-                <>
-                  <TeamInfoItem
-                    key={item.gameId}
-                    awayTeam={item.awayTeam}
-                    homeTeam={item.homeTeam}
-                    gameDate={item.gameDate}
-                    voteTeamId={item.voteTeamId}
-                  />
-                </>
-              )),
-            )}
+            fifthData[0]?.games?.map((item) => (
+              <>
+                <TeamInfoItem
+                  key={item.gameId}
+                  awayTeam={item.awayTeam}
+                  homeTeam={item.homeTeam}
+                  voteTeamId={item.voteTeamId}
+                />
+              </>
+            ))}
+          <hr />
+          <Text medium>
+            {fifthData && fifthData[1]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fifthData &&
+            fifthData[1]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          <hr />
+          <Text medium>
+            {fifthData && fifthData[2]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fifthData &&
+            fifthData[2]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          {fifthData[3] === undefined || <hr />}
+          <Text medium>
+            {fifthData && fifthData[3]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fifthData &&
+            fifthData[3]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          {fifthData[4] === undefined || <hr />}
+          <Text medium>
+            {fifthData && fifthData[4]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fifthData &&
+            fifthData[4]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
+          {fifthData[5] === undefined || <hr />}
+          <Text medium>
+            {fifthData && fifthData[5]?.games?.find((item) => item)?.gameDate}
+          </Text>
+          {fifthData &&
+            fifthData[5]?.games?.map((item) => (
+              <TeamInfoItem
+                key={item.gameId}
+                awayTeam={item.awayTeam}
+                homeTeam={item.homeTeam}
+                voteTeamId={item.voteTeamId}
+              />
+            ))}
         </ul>
       );
     }
