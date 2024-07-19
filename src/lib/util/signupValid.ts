02a793414904dd78d1cba3a8fa51f590fc4f707b
doitidey <evxrafter@gmail.com>
2024-07-20 01:07:39 +0900

    Fix: 마이페이지 닉네임 글자수 체크 수정

--- src/lib/util/signupValid.ts
+++ src/lib/util/signupValid.ts
@@ -1,4 +1,4 @@
 export const rInputRegexs = {
-  nicknameRegex: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{1,6}$/,
+  nicknameRegex: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,6}$/,
   commentRegex: /^(?=.*){0,89}$/,
 };
