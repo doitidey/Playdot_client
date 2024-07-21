6520c91c6cd91ce58b20207897050a2777c34f93
doitidey <evxrafter@gmail.com>
2024-07-21 20:53:47 +0900

    Fix: 마이페이지 요정기록 수정

--- src/components/mypage/MypageFairyRecord.tsx
+++ src/components/mypage/MypageFairyRecord.tsx
@@ -12,55 +12,64 @@ interface HistoryData {
 
 function MypageFairyRecord() {
   const [historyData, setHistoryData] = useState<HistoryData[]>();
+  const [victoryData, setVictoryData] = useState<HistoryData[]>();
+  const [loseData, setLoseData] = useState<HistoryData[]>();
+
   useEffect(() => {
     const getHistoryData = async () => {
       const res = await getFairyHistory();
       setHistoryData(res.data);
+
+      const victory = res.data.filter((data: HistoryData) =>
+        data.title.includes("승리"),
+      );
+      const lose = res.data.filter((data: HistoryData) =>
+        data.title.includes("패배"),
+      );
+      setVictoryData(victory);
+      setLoseData(lose);
     };
+
     getHistoryData();
   }, []);
 
-  const victoryData = historyData?.filter((data) => {
-    data.title.includes("승리요정 1등");
-  });
-
-  const loseData = historyData?.filter((data) => {
-    data.title.includes("패배요정 1등");
-  });
+  console.log(historyData);
 
   return (
     <div className="fairy">
       <div className="fairy__content">
-        <Title large className="fairy__title">
-          승리요정
-        </Title>
         {victoryData && victoryData.length > 0 ? (
-          <>
-            <div className="medal">
-              <Image
-                className="medal__confeti"
-                src={"images/confetti.svg"}
-                alt={"컨페티"}
-                width={394}
-                height={132}
-              />
-              <Image
-                className="medal__medalbg"
-                src={"images/fairypolygon.svg"}
-                alt={"메달"}
-                width={380}
-                height={400}
-              />
-              <Image
-                className="medal__number"
-                src={`images/${victoryData[0].count}.svg`}
-                alt={"넘버"}
-                width={80}
-                height={92}
-              />
-            </div>
-            <p>총 3회 달성</p>
-          </>
+          victoryData.map((i) => (
+            <>
+              <Title large className="fairy__title">
+                {i.title}
+              </Title>
+              <div className="medal">
+                <Image
+                  className="medal__confeti"
+                  src={"images/confetti.svg"}
+                  alt={"컨페티"}
+                  width={394}
+                  height={132}
+                />
+                <Image
+                  className="medal__medalbg"
+                  src={"images/fairypolygon.svg"}
+                  alt={"메달"}
+                  width={380}
+                  height={400}
+                />
+                <Image
+                  className="medal__number"
+                  src={`images/${i.count}.svg`}
+                  alt={"넘버"}
+                  width={80}
+                  height={92}
+                />
+              </div>
+              <p>총 {i.count}회 달성</p>
+            </>
+          ))
         ) : (
           <>
             <p>아직 기록이 없어!</p>
@@ -68,36 +77,38 @@ function MypageFairyRecord() {
         )}
       </div>
       <div className="fairy__content">
-        <Title large className="fairy__title">
-          패배요정
-        </Title>
         {loseData && loseData.length > 0 ? (
-          <>
-            <div className="medal">
-              <Image
-                className="medal__confeti"
-                src={"images/confetti.svg"}
-                alt={"컨페티"}
-                width={394}
-                height={132}
-              />
-              <Image
-                className="medal__number"
-                src={`images/${loseData[0].count}.svg`}
-                alt={"넘버"}
-                width={80}
-                height={92}
-              />
-              <Image
-                className="medal__medalbg"
-                src={"images/fairypolygon.svg"}
-                alt={"메달"}
-                width={380}
-                height={400}
-              />
-            </div>
-            <p>총 3회 달성</p>
-          </>
+          loseData.map((i) => (
+            <>
+              <Title large className="fairy__title">
+                {i.title}
+              </Title>
+              <div className="medal">
+                <Image
+                  className="medal__confeti"
+                  src={"images/confetti.svg"}
+                  alt={"컨페티"}
+                  width={394}
+                  height={132}
+                />
+                <Image
+                  className="medal__number"
+                  src={`images/${i.count}.svg`}
+                  alt={"넘버"}
+                  width={80}
+                  height={92}
+                />
+                <Image
+                  className="medal__medalbg"
+                  src={"images/fairypolygon.svg"}
+                  alt={"메달"}
+                  width={380}
+                  height={400}
+                />
+              </div>
+              <p>총 {i.count}회 달성</p>
+            </>
+          ))
         ) : (
           <p>아직 기록이 없어!</p>
         )}
